44b4c8811e82ea432718fc8159a7b0b1621fcccf
root <root@DESKTOP-5G3VHUO.localdomain>
2022-02-02 16:01:14 -0500

    finished part 2

--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -1,5 +1,7 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
+package csci2020u.lab02;
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
